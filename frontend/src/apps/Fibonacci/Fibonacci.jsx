3dc09806beb51f033b68eeac0b629d51366444ce
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-06-18 11:29:41 -0300

    refactor: Fibonacci page

--- frontend/src/apps/Fibonacci/Fibonacci.jsx
+++ frontend/src/apps/Fibonacci/Fibonacci.jsx
@@ -23,6 +23,12 @@ const Fibonacci = () => {
       </header>
 
       <main>
+        <blockquote cite="https://pt.wikipedia.org/wiki/Sequ%C3%AAncia_de_Fibonacci">
+          "Na matemática, a sequência de Fibonacci, é uma sequência de números inteiros, começando normalmente por 0 e 1, na qual cada termo subsequente corresponde à soma dos dois anteriores."
+          <small>Fonte:
+            <a href="https://pt.wikipedia.org/wiki/Sequ%C3%AAncia_de_Fibonacci" target="_blank" rel="noreferrer">Wikipedia</a>
+          </small>
+        </blockquote>
         <h2>Digite um número e descubra se ele pertence à sequência de Fibonacci.</h2>
 
         <form onSubmit={ verificarFibonacci }>
