99d34b05142fc958073b6bc81623f175ab817612
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-06-18 20:58:52 -0300

    feat: add Swagger'

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
-const router = require('./apps/router');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 const ErrorMiddleware = require('./middlewares/error');
+const router = require('./apps/router');
 
 const app = express();
 
 app.use(cors());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', router);
 app.use(ErrorMiddleware);
 
