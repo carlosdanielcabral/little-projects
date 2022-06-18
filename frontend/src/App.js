1879c9d20c2128cf48e399dd8c290c0ff564a11c
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-06-18 11:29:21 -0300

    feat: add routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './apps/Home/Home';
 import Fibonacci from './apps/Fibonacci/Fibonacci';
+import Faturamento from './apps/Faturamento/Faturamento';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/fibonacci" component={Fibonacci} />
+        <Route exact path="/faturamento" component={Faturamento} />
       </Switch>
     </Router>
   );
