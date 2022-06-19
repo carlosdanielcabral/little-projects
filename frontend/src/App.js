538f7df3877766656ce4d457e49d03ae3160a9fd
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-06-18 21:10:42 -0300

    feat: frontend eslint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './apps/Home/Home';
 import Fibonacci from './apps/Fibonacci/Fibonacci';
 import Faturamento from './apps/Faturamento/Faturamento';
@@ -6,17 +6,17 @@ import Percentual from './apps/Percentual/Percentual';
 import InverterString from './apps/InverterString/InverterString';
 
 const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/fibonacci" component={Fibonacci} />
-        <Route exact path="/faturamento" component={Faturamento} />
-        <Route exact path="/percentual" component={Percentual} />
-        <Route exact path="/inverter-strings" component={InverterString} />
-      </Switch>
-    </Router>
-  );
-}
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/fibonacci" component={Fibonacci} />
+                <Route exact path="/faturamento" component={Faturamento} />
+                <Route exact path="/percentual" component={Percentual} />
+                <Route exact path="/inverter-strings" component={InverterString} />
+            </Switch>
+        </Router>
+    );
+};
 
 export default App;
